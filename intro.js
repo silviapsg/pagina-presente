9eb99599873dbd6cc7730d4a59ec1f8533a79ce6
Sílvia Patrícia <silviapsg93@gmail.com>
2025-08-29 11:37:04 -0400

    Criação da página introdutória e arquivos relacionados

---
+++ intro.js
@@ -0,0 +1,14 @@
+const dataLimite = new Date(2025, 7, 29, 11, 59);
+const agora = new Date();
+
+const botao = document.getElementById("entrar");
+const expirado = document.getElementById("expirado");
+
+if (agora > dataLimite) {
+    botao.style.display = "none";
+    expirado.style.display = "block";
+} else {
+    botao.addEventListener("click", () => {
+        window.location.href = "index.html";
+    });
+}
